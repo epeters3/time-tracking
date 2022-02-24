a1d5b35c75876d09006543147d3581781f71f558
Evan Peterson <evanpeterson17@gmail.com>
2022-02-23 19:58:06 -0700

    Reduce complexity, and improve error messages

--- tracker/__main__.py
+++ tracker/__main__.py
@@ -63,38 +63,46 @@ class Tracker:
             self.total_time += now - self.last_check
         self.last_check = now
 
-    def _get_header_str(self, command: str):
+    def _get_header_str(self, command: t.Optional[str]):
+        """
+        Creates a header string to display at the beginning of each response to user input. `command` is the user's
+        command. If it is `None`, it is assumed that their command was invalid, and an error status is included in the
+        header string.
+        """
         now = datetime.now().strftime("%m-%d-%Y %I:%M:%S %p")
-        return f"{Color.OKGREEN}[{command.upper()}]{Color.ENDC} {Color.OKBLUE}[{now}] [{self.total_time}]{Color.ENDC}"
+        if command:
+            cmd_str = f"{Color.OKGREEN}[{command.upper()}]{Color.ENDC}"
+        else:
+            cmd_str = f"{Color.FAIL}[ERROR]{Color.ENDC}"
+        return f"{cmd_str} {Color.OKBLUE}[{now}] [{self.total_time}]{Color.ENDC}"
 
     def _process_user_input(self, user_input: str):
         """Execute the command associated with `user_input`."""
-        if " " in user_input:
-            args = user_input.split(" ")
-            user_cmd, args = args[0], args[1:]
-        else:
-            user_cmd = user_input
-            args = []
+        args = user_input.split(" ")
+        user_cmd, args = args[0], args[1:]
+
         command = None
         for cmd, names in self.command_map.items():
             if user_cmd in names:
                 command = cmd
 
         if command is None:
-            print(f"invalid user input {user_input}")
+            msg = f"Invalid user input \"{user_input}\""
         else:
             msg = getattr(self, command)(args)
-            output = self._get_header_str(command)
-            if msg:
-                output += f" {msg}"
-            print(output)
+
+        return command, msg
 
     def interact(self):
         print("Welcome to time tracking, please enter a command (press 'h' for help).")
         while True:
             user_input = input(">>> ")
             self._tick()
-            self._process_user_input(user_input)
+            command, msg = self._process_user_input(user_input)
+            output = self._get_header_str(command)
+            if msg:
+                output += f" {msg}"
+            print(output)
 
 
 if __name__ == "__main__":
