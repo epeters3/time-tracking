17bdf0116a4a8826d98dd0c777c091a904d93464
Evan Peterson <evanpeterson17@gmail.com>
2022-02-23 19:46:52 -0700

    Simplify tracking mechanism, and print time tracked so far at each command

--- tracker/__main__.py
+++ tracker/__main__.py
@@ -3,67 +3,40 @@ import sys
 import typing as t
 from datetime import datetime, timedelta
 
+from color import Color
+
 
 class Tracker:
     command_map = {
-        "start": {"s", "start", "r", "resume"},
+        "start": {"s", "start", "begin", "r", "resume"},
         "add": {"a", "add"},
         "check": {"check", "c"},
         "pause": {"p", "pause"},
-        "end": {"end", "e"},
         "help": {"help", "h"},
         "quit": {"q", "quit", "exit"}
     }
 
-    def __init__(self) -> None:
+    def __init__(self):
         self.total_time = timedelta()
-        self.start_time = None
+        self.last_check = datetime.now()
         self.is_tracking = False
 
-    def start(self, _) -> None:
-        """
-        Begin tracking time.
-        """
-        self.start_time = datetime.now()
+    def start(self, _):
+        """Begin tracking time."""
         self.is_tracking = True
-        print(f"Started timer at {datetime.now()}")
+        return "Started timer."
 
-    def check(self, _) -> None:
-        """
-        Check how much time you've tracked so far.
-        """
-        if not self.is_tracking:
-            print("You need to start a timer before you can check it.")
-            return
+    def check(self, _):
+        """Check how much time you've tracked so far."""
+        pass
 
-        print(
-            f"You've tracked {self.total_time + (datetime.now() - self.start_time)} time so far."
-        )
-
-    def pause(self, _) -> None:
-        """
-        Pause the time tracker.
-        """
-        if not self.is_tracking:
-            print("You need to start a timer before you can pause it.")
-            return
-
-        self.total_time += datetime.now() - self.start_time
-        self.start_time = None
+    def pause(self, _):
+        """Pause the time tracker."""
         self.is_tracking = False
-        print(f"Tracker paused. You've tracked {self.total_time} time so far.")
+        return "Tracker paused."
 
-    @staticmethod
-    def end(_) -> None:
-        """
-        Pause, save, and quit the time tracking process.
-        """
-        print("This command is not implemented yet.")
-
-    def help(self, _) -> None:
-        """
-        Print a description of the time tracker's commands.
-        """
+    def help(self, _):
+        """Print a description of the time tracker's commands."""
         for cmd, names in self.command_map.items():
             print(f"{cmd} (aliases={names}): {getattr(self, cmd).__doc__}")
 
@@ -77,16 +50,25 @@ class Tracker:
         hours = int(m.group(1))
         minutes = int(m.group(2))
         self.total_time += timedelta(hours=hours, minutes=minutes)
-        print(f"added {time_str} to timer. You've tracked {self.total_time} time so far.")
+        return f"Added {time_str} to timer."
 
     @staticmethod
     def quit(_):
         sys.exit(0)
 
+    def _tick(self):
+        """Updates the tracked time."""
+        now = datetime.now()
+        if self.is_tracking:
+            self.total_time += now - self.last_check
+        self.last_check = now
+
+    def _get_header_str(self, command: str):
+        now = datetime.now().strftime("%m-%d-%Y %I:%M:%S %p")
+        return f"{Color.OKGREEN}[{command.upper()}]{Color.ENDC} {Color.OKBLUE}[{now}] [{self.total_time}]{Color.ENDC}"
+
     def _process_user_input(self, user_input: str):
-        """
-        Execute the command associated with `user_input`.
-        """
+        """Execute the command associated with `user_input`."""
         if " " in user_input:
             args = user_input.split(" ")
             user_cmd, args = args[0], args[1:]
@@ -101,12 +83,17 @@ class Tracker:
         if command is None:
             print(f"invalid user input {user_input}")
         else:
-            getattr(self, command)(args)
+            msg = getattr(self, command)(args)
+            output = self._get_header_str(command)
+            if msg:
+                output += f" {msg}"
+            print(output)
 
-    def interact(self) -> None:
+    def interact(self):
         print("Welcome to time tracking, please enter a command (press 'h' for help).")
         while True:
             user_input = input(">>> ")
+            self._tick()
             self._process_user_input(user_input)
 
 
